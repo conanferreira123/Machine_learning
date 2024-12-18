0f4ae5b8edbd36962487ab0531467c337dd9ec74
conanferreira123 <conanferreira196@gmail.com>
2024-12-19 01:31:52 +0530

    new update

--- house_price_pred.py
+++ house_price_pred.py
@@ -1,1 +1,10 @@
-print("hello github")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+
+
+df=pd.read_csv('canada_per_capita_income.csv')
+print(df.columns)
+max=df['year']['per capita income (US$)'==[df['per capita income (US$)'].max()]]
+print(max)
